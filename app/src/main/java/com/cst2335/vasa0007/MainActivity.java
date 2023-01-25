f2e2d1979a1ed0ae5a5e77c7a29da29dab66e468
Mathunan Vasanthan <vasa0007algonquinlive.com>
2023-01-24 19:23:28 -0500

    Add buttons,chekbox,switch with snackbars and toast in both language.

--- app/src/main/java/com/cst2335/vasa0007/MainActivity.java
+++ app/src/main/java/com/cst2335/vasa0007/MainActivity.java
@@ -1,14 +1,50 @@
 package com.cst2335.vasa0007;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+
+        Switch swt = findViewById(R.id.switch1);
+        swt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean b) {
+                if(cb.isChecked()){
+                    Snackbar snackbar1= Snackbar.make(swt,getResources().getString(R.string.on),Snackbar.LENGTH_LONG).setAction(getResources().getString(R.string.undo),click ->
+                            cb.setChecked(!b));
+                    snackbar1.show();
+                }else {
+                    Snackbar snackbar2= Snackbar.make(swt,getResources().getString(R.string.off),Snackbar.LENGTH_LONG).setAction(getResources().getString(R.string.undo),click ->
+                            cb.setChecked(!b));
+                    snackbar2.show();
+                }
+            }
+        });
+
+
+
+
+    }
+
+
+    public void clickListener(View view) {
+        Toast.makeText(this, getResources().getString(R.string.toast_message), Toast.LENGTH_SHORT).show();
     }
 }
